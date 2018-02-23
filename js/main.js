92d84f76d22bee0d2d3b9029654834ada0267139
Anton <almosoww@gmail.com>
2018-02-23 19:21:15 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,39 +1,18 @@
-$("span").click(function() {
-  $( "#aboutUs" ).show( "slow", function() {
+$(document).ready(function(){
+  let owl = $(".owl-carousel"),
+      prev = $("#left")
+      next = $("#right")
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    center: true,
+    next: $("#right"),
+    prev: $("#left")
+  });
+  $("#left").on("click", ()=>{
+    owl.trigger("prev.owl.carousel");
+  });
+  $("#right").on("click", ()=>{
+    owl.trigger("next.owl.carousel");
   });
 });
-$('a[href*="#"]')
-  // Remove links that don't actually link to anything
-  .not('[href="#"]')
-  .not('[href="#0"]')
-  .click(function(event) {
-    // On-page links
-    if (
-      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-      &&
-      location.hostname == this.hostname
-    ) {
-      // Figure out element to scroll to
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      // Does a scroll target exist?
-      if (target.length) {
-        // Only prevent default if animation is actually gonna happen
-        event.preventDefault();
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, function() {
-          // Callback after animation
-          // Must change focus!
-          var $target = $(target);
-          $target.focus();
-          if ($target.is(":focus")) { // Checking if the target was focused
-            return false;
-          } else {
-            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-            $target.focus(); // Set focus again
-          };
-        });
-      }
-    }
-  });
